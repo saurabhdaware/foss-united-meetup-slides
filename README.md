4ba782f09584afbcb217b36e53f6a4db91ad1fda
saurabhdaware <saurabhdaware99@gmail.com>
2023-12-08 23:21:00 +0530

    feat: init

---
+++ README.md
@@ -0,0 +1,16 @@
+# React India 2023 Goa Slides
+
+These are my slides for React India 2023 talk "In The World of JS Frameworks, It's an HTML Framework!"
+
+## Local Setup
+
+```sh
+# Install dependencies
+pnpm install
+
+# Dev Server
+pnpm dev
+
+# To create build
+pnpm build
+```
---
+++ client.ts
@@ -0,0 +1,57 @@
+import Reveal from 'reveal.js';
+// import RevealHighlight from 'reveal.js/plugin/highlight/highlight';
+import 'reveal.js/dist/reveal.css';
+import RevealNotes from 'reveal.js/plugin/notes/notes';
+import 'reveal.js/dist/theme/white.css';
+import 'highlight.js/styles/github.css';
+import './slides.css';
+
+let deck = new Reveal({
+  plugins: [RevealNotes],
+  hash: true,
+  transition: 'none',
+  controls: false,
+  transitionSpeed: 'fast',
+  autoAnimateDuration: 0.3,
+  navigationMode: 'linear',
+})
+
+deck.initialize();
+
+let timerInterval; // Variable to store the interval ID
+
+function startTimer() {
+  let timerElement = document.getElementById('timer-count');
+  let seconds = 1;
+  let minutes, displaySeconds;
+
+  timerInterval = setInterval(function () {
+    // @ts-expect-error
+    minutes = parseInt(seconds / 60, 10);
+    displaySeconds = seconds % 60;
+
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    displaySeconds = displaySeconds < 10 ? '0' + displaySeconds : displaySeconds;
+
+    timerElement.textContent = minutes + ':' + displaySeconds;
+
+    seconds++;
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(timerInterval);
+  timerInterval = undefined;
+}
+
+const buttonEl = document.querySelector<HTMLButtonElement>('button.timer');
+buttonEl.addEventListener('click', (e) => {
+  console.log(buttonEl);
+  if (timerInterval) {
+    stopTimer()
+    buttonEl.textContent = 'Start Timer'
+  } else {
+    startTimer();
+    buttonEl.textContent = 'Stop Timer'
+  }
+})
